a1f35da09715843b2a5ca774ce636e422785735f
jtc1246 <jtc1246@outlook.com>
2024-07-23 19:06:27 +0800

    fix update in cors header

--- openai_playground/main.py
+++ openai_playground/main.py
@@ -328,7 +328,7 @@ class Request(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-Length', 0)
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Headers', 'openai-organization,content-type,authorization')
+        self.send_header('Access-Control-Allow-Headers', 'openai-organization,content-type,authorization,openai-project')
         self.end_headers()
         self.wfile.flush()
     
